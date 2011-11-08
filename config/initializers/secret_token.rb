15f50e084df012cb5b0e9e8c2fb012e6430cd2d4
Nelio Carneiro <nelio@N-Note.localdomain>
2011-11-08 12:55:39 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portugon::Application.config.secret_token = 'b4bb5d0babec3f676da0a68f4d395db1baad6604e3d33e849a50b6a0020d7072406e4fd13bb17de85184df1185130c560eee24376a804814559135b8ac078062'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Portugon::Application.config.session_store :cookie_store, :key => '_portugon_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Portugon::Application.config.session_store :active_record_store
